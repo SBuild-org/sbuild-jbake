6343d0d6780e8a8c49714362a92c19ae89ed9400
Tobias Roeser <le.petit.fou@web.de>
2013-12-20 11:54:09 +0100

    Use updated sbuild-plugin.

--- org.sbuild.plugins.jbake/SBuild.scala
+++ org.sbuild.plugins.jbake/SBuild.scala
@@ -4,18 +4,14 @@ import de.tototec.sbuild.ant.tasks._
 
 import de.tototec.sbuild._
 
-@version("0.7.0")
-@classpath("../../sbuild-plugin/org.sbuild.plugins.sbuildplugin/target/org.sbuild.plugins.sbuildplugin-0.1.0.jar")
+@version("0.7.1")
+@classpath("../../sbuild-plugin/org.sbuild.plugins.sbuildplugin/target/org.sbuild.plugins.sbuildplugin-0.1.0.9000.jar")
 class SBuild(implicit _project: Project) {
 
-  Plugin[org.sbuild.plugins.sbuildplugin.SBuildPlugin] configure { c =>
-    import c._
-    // Note. We require at least 0.7.0.9000 from the immutable-plugin-config branch, as we use an immutable config class.
-    sbuildVersion = "0.7.0"
-
-    pluginClass = "org.sbuild.plugins.jbake.JBake"
+  Plugin[org.sbuild.plugins.sbuildplugin.SBuildPlugin] configure { _.copy(
+    sbuildVersion = "0.7.1",
+    pluginClass = "org.sbuild.plugins.jbake.JBake",
     pluginVersion = "0.0.9000"
-    pluginFactoryClass = "org.sbuild.plugins.jbake.JBakePlugin"
-  }
+  )}
 
 }
