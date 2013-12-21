4669308b3f3cc4c46d5f7739e5ccb56dc3a478c5
Tobias Roeser <le.petit.fou@web.de>
2013-12-21 15:14:30 +0100

    Use publicaly release sbuildplugin-plugin 0.2.0

--- org.sbuild.plugins.jbake/SBuild.scala
+++ org.sbuild.plugins.jbake/SBuild.scala
@@ -5,7 +5,7 @@ import de.tototec.sbuild.ant.tasks._
 import de.tototec.sbuild._
 
 @version("0.7.1")
-@classpath("../../sbuild-plugin/org.sbuild.plugins.sbuildplugin/target/org.sbuild.plugins.sbuildplugin-0.1.0.9000.jar")
+@classpath("mvn:org.sbuild:org.sbuild.plugins.sbuildplugin:0.2.0")
 class SBuild(implicit _project: Project) {
 
   Plugin[org.sbuild.plugins.sbuildplugin.SBuildPlugin] configure { _.copy(
