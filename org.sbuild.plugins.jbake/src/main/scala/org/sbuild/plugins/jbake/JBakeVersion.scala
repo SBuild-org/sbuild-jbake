d9419af132088f2b52d3eb15eb4c5f401c967521
Tobias Roeser <le.petit.fou@web.de>
2014-01-28 22:18:16 +0100

    Added some more config options to JBakeVersion and updated Readme.

--- org.sbuild.plugins.jbake/src/main/scala/org/sbuild/plugins/jbake/JBakeVersion.scala
+++ org.sbuild.plugins.jbake/src/main/scala/org/sbuild/plugins/jbake/JBakeVersion.scala
@@ -8,10 +8,11 @@ import de.tototec.sbuild.TargetRefs.fromString
 
 /**
  * A configuration of a JBake instance.
- * There are two different flavours:
+ * There are three different flavours:
  * - `[[JBakeVersion$.Packaged]]` - A JBake distribution ZIP, defined by `version` and `url` (which can be any `[[TargetRef]]`)
  * - `[[JBakeVersion$.Local]]` -- A locally installed JBake, defined by a `homeDir`.
- * 
+ * - `[[JBakeVersion$.Classpath]]` -- JBake will completely resolved from the given classpath, which is a `[[TargetRefs]]`.
+ *
  * Note, there are also some predefined versions available in `[[JBakeVersion$]]`.
  */
 sealed trait JBakeVersion {
@@ -19,9 +20,16 @@ sealed trait JBakeVersion {
 }
 
 object JBakeVersion {
-  /** 
-   *  A JBake distribution ZIP 
-   *  
+
+  object Packaged {
+    /**
+     * Conveniently download and use a released version from `jbake.org`.
+     */
+    def apply(version: String): JBakeVersion = Packaged(version, s"http://jbake.org/files/jbake-${version}-bin.zip")
+  }
+  /**
+   *  A JBake distribution ZIP.
+   *
    *  @param version The JBake version this package contains.
    *  @param url The URL to the package. This parameter supports also any [[TargetRef.name]].
    */
@@ -30,17 +38,25 @@ object JBakeVersion {
   }
   /**
    * A locally installed (or at least unpacked) JBake.
-   * 
+   *
    * @param homeDir The installation directory of the locally installed JBake version.
    */
   case class Local(homeDir: File) extends JBakeVersion {
     override def classpath(implicit p: Project): TargetRefs = s"${homeDir}/jbake-core.jar" ~ s"scan:${homeDir}/lib;regex=.*\.jar"
   }
-  
+  /**
+   * The JBake runtime is completely resolved from the given `explicitClasspath`.
+   *
+   * @param explicitClasspath The classpath, from where the JBake runtime will be loaded.
+   */
+  case class Classpath(explicitClasspath: TargetRefs) extends JBakeVersion {
+    override def classpath(implicit p: Project): TargetRefs = explicitClasspath
+  }
+
   /** JBake 2.2.0 */
-  val JBake_2_2_0 = Packaged("2.2.0", "http://jbake.org/files/jbake-2.2.0-bin.zip")
+  val JBake_2_2_0 = Packaged("2.2.0")
 
   /** JBake 2.2.1 */
-  val JBake_2_2_1 = Packaged("2.2.1", "http://jbake.org/files/jbake-2.2.1-bin.zip")
+  val JBake_2_2_1 = Packaged("2.2.1")
 
 }
